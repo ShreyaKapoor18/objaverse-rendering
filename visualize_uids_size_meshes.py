ff4734bf0c0d775aa731426e21afe6828e595714
ShreyaKapoor18 <kapoorshreya18@gmail.com>
2023-08-01 13:59:23 +0200

    working

--- visualize_uids_size_meshes.py
+++ visualize_uids_size_meshes.py
@@ -4,9 +4,12 @@ import numpy as np
 import bpy
 import math
 from mathutils import Vector
-
+from mathutils import Matrix
+from mathutils import Vector
 
 bproc.init()
+
+
 
 def normalize_scene(scene):
     bbox_min, bbox_max = scene_bbox(scene)
@@ -43,7 +46,6 @@ def scene_root_objects(scene):
     for obj in scene:
         if not obj.blender_obj.parent:
             yield obj
-
 def scene_meshes(scene):
     for obj in scene:
         print(dir(obj))
@@ -57,30 +59,20 @@ for object in glob.glob('objects/glbs/*/*'):
     print(object_name)
 
     scene = bproc.loader.load_obj(object)
-    light = bproc.types.Light()
-    light.set_location([2, -2, 0])
-    light.set_energy(300)
-    normalize_scene(scene)
     # Set the camera to be in front of the object
-    # Find point of interest, all cam poses should look towards it
-    poi = bproc.object.compute_poi(scene)
-    # Sample random camera location above objects
+    # Find point of interest, all cam poses should look towards it    # Sample random camera location above objects
     # Find point of interest, all cam poses should look towards it
     # Sample five camera poses
-    for i in range(5):
-    # Sample random camera location above objects
-        location = np.random.uniform([-5, -5, -5], [5, 5, 5])
-        # Compute rotation based on vector going from location towards poi
-        rotation_matrix = bproc.camera.rotation_from_forward_vec(poi - location, inplane_rot=np.random.uniform(-0.3, 0.3))
-        # Add homog cam pose based on location an rotation
-        cam2world_matrix = bproc.math.build_transformation_mat(location, rotation_matrix)
-        bproc.camera.add_camera_pose(cam2world_matrix)
+    bproc.camera.set_intrinsics_from_blender_params(1, 512, 512, lens_unit="FOV")
+    normalize_scene(scene)
+        
 
     # Render the scene
     data = bproc.renderer.render()
     # Write the rendering into an hdf5 file
-    bproc.writer.write_hdf5(f'output/{object_name}', data)
-    #bproc.writer.write_hdf5(f"output/{object_name}", data)
+    #bproc.write_gif(f'output/{object_name}', data)
+    bproc.writer.write_hdf5(f"output/{object_name}", data)
     bproc.clean_up()
+    
 
 
