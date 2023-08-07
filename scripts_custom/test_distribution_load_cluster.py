79089c55fdd3f8cb0d838faabb287f8e26fae607
ShreyaKapoor18 <kapoorshreya18@gmail.com>
2023-08-07 09:54:13 +0200

    results on alex

--- scripts_custom/test_distribution_load_cluster.py
+++ scripts_custom/test_distribution_load_cluster.py
@@ -12,7 +12,34 @@ import os
 from os.path import join
 from multiprocessing import Pool
 #%
+
+"""
+This program counts the number of meshes in the 
+scene. It imports each scene and then checks the number of
+meshes in the scene. Then we get the names of the objects in the folder
+which has n = 5 meshes, n=20 meshes, n= 20 and n=100 meshes accordingly
+"""
+
+def reset_scene() -> None:
+    """Resets the scene to a clean state."""
+    # delete everything that isn't part of a camera or a light
+    for obj in bpy.data.objects:
+        if obj.type not in {"CAMERA", "LIGHT"}:
+            bpy.data.objects.remove(obj, do_unlink=True)
+    # delete all the materials
+    for material in bpy.data.materials:
+        bpy.data.materials.remove(material, do_unlink=True)
+    # delete all the textures
+    for texture in bpy.data.textures:
+        bpy.data.textures.remove(texture, do_unlink=True)
+    # delete all the images
+    for image in bpy.data.images:
+        bpy.data.images.remove(image, do_unlink=True)
+
 def count_meshes(objs):
+    # reset the scene before every import so that there is no object in the scene
+    # this will prevent the ram from being overloaded and lead to a better functioning program
+    reset_scene()
     print(objs)
     if objs.endswith(".glb"):
         bpy.ops.import_scene.gltf(filepath=objs) # why is this giving an error? loading the file should be easy
@@ -48,9 +75,10 @@ def count_meshes(objs):
 if __name__ == '__main__':
     num_cores = int(os.getenv("SLURM_CPUS_PER_TASK"))# reptetitively giving one object, why???
     objaverse_dir = '/home/janus/iwi9-datasets/objaverse-objects/hf-objaverse-v1/glbs/*/*'
-
+    print(os.getcwd())
     object_dir = list(glob.glob(objaverse_dir))
-    f = open('../extras/list_lvis.txt')
+    base_dir = '../objaverse-rendering/'
+    f = open(join(base_dir,'extras/list_lvis.txt'), 'r')
     list_lvis = []
     for line in f.readlines():
         line = line.strip('\n')
