1b1e478f3df94e06e60ec28657e6a7352a032ddf
ShreyaKapoor18 <kapoorshreya18@gmail.com>
2023-08-10 14:36:05 +0200

    generate json out of mem

--- scripts_custom/generate_json_from_text.py
+++ scripts_custom/generate_json_from_text.py
@@ -22,18 +22,19 @@ base_dir = '~/git/objaverse-rendering/'
 file = open(join('results', 'test_5.txt'), 'r')
 objaverse_dir = '/home/janus/iwi9-datasets/objaverse-objects/hf-objaverse-v1'
 object_paths = list(objaverse._load_object_paths().values())
-print(object_paths[:10])
 
 global list_paths
 list_paths = []
 
-num_cores = int(os.getenv("SLURM_CPUS_PER_TASK")) - 1
+#num_cores = int(os.getenv("SLURM_CPUS_PER_TASK")) - 1
+num_cores = 2
+print(num_cores)
 for name in file.read():
-    name = name.strip('\n') + '.glb'   
+    name_2 = name.strip('\n') + '.glb'   
     p = Pool(num_cores)
-    list_paths = p.map(find, itertools.repeat(name), object_paths)
-    p.close()
+    list_paths = p.map(find, itertools.repeat(name_2), object_paths)
     p.join()
+    p.close()
     gc.collect()
     
 with open(join('input_models_path_5.txt'), 'w') as f:
